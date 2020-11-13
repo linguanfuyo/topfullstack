8d34b29668114c9411c534223fbfe39a8f56beeb
linguanfu <921198421@qq.com>
2020-11-14 00:50:20 +0800

    发布视频页面 表单保存视频模板

--- web/pages/user/userCreate.vue
+++ web/pages/user/userCreate.vue
@@ -6,6 +6,14 @@
         <a-upload-dragger ref="upload" :headers="getAuthHeaders()" :showUploadList="false" name="file" :multiple="false"
           action="http://localhost:3008/upload" @change="handleChange">
           <div class="upload-dragg" style="height: 500px;background-color: #ffff">
+            <div @click.stop="" class="templateTips" v-if="isHaveTemplate">
+              <span style="margin:1px 8px 0 20px">
+                <a-icon style="font-size: 16px;color:#798499" theme="filled" type="video-camera" />
+              </span>
+              <span>你还有上次未提交的视频，是否继续编辑？</span>
+              <span @click.stop="()=>{isUploaded = false}" style="color:#4e81d9;margin:0 15px 0 30px">继续编辑</span>
+              <span @click.stop="deleteTemplate">放弃</span>
+            </div>
             <div class="center">
               <a-icon style="font-size: 150px;color:#e9e8e9" type="cloud-upload" />
               <div class="center-tips">点击上传或将文件拖入此区域</div>
@@ -55,15 +63,15 @@
     <div v-if="!isUploaded" class="display">
       <div class="dispay-header">
         <div class="video-info">
-          <span class="video-name">{{viedoInfo.name}}</span>
+          <span class="video-name">{{videoInfo.name}}</span>
           <span @click="changeUpload" class="video-btn">更换视频</span>
         </div>
         <div class="dispay-info">
           <div>
             <span class="dispay-lenght" style="padding-right: 40px">视频时长 <span
-                style="font-weight:bold;color:black">{{formatTime(viedoInfo.timeStamp)}}秒</span></span>
+                style="font-weight:bold;color:black">{{durationTrans(videoInfo.duration)}}</span></span>
             <span
-              class="dispay-lenght">上传详情：{{formatFileSize(viedoInfo.loaded)}}/{{formatFileSize(viedoInfo.size)}}</span>
+              class="dispay-lenght">上传详情：{{formatFileSize(videoInfo.loaded)}}/{{formatFileSize(videoInfo.size)}}</span>
           </div>
           <div v-if="!isComplete" class="percent">
             {{percen}}%
@@ -78,11 +86,53 @@
               <span>上传失败</span>
             </span>
           </div>
-
         </div>
       </div>
       <div class="progress">
-        <v-progress-linear color="light-green darken-4" height="5" :value="percen" striped></v-progress-linear>
+        <v-progress-linear color="light-green darken-1" height="5" :value="percen" striped></v-progress-linear>
+      </div>
+      <!-- 视频长度比提示 -->
+      <a-alert banner style="margin: 30px 30px 0px 30px; font-size: 12px;color:#222222" v-if="visible"
+        message="竖版视频（宽<高的视频，如9:16）不享受收益分成，不支持参与活动，无原创、赞赏、定时等功能" type="error" closable :after-close="handleClose" />
+      <!-- 视频表单 -->
+      <div class="video-form" style="margin: 0 30px">
+        <div class="input-item">
+          <div style="width:50px; margin-right: 40px" for="title"><span style="color:red">* </span>标题</div>
+          <a-input v-model="videoModel.title" size="large" :maxLength="30" />
+        </div>
+        <div class="input-item">
+          <div style="width:50px; margin-right: 40px" for="title"><span style="color:red">* </span>封面</div>
+          <a-upload name="file" list-type="picture-card" class="avatar-uploader" :show-upload-list="false"
+            action="http://localhost:3008/upload" :headers="getAuthHeaders()" :before-upload="beforeUpload"
+            @change="handleChange2">
+            <img style="height: 100px;width: 150px" v-if="videoModel.cover" :src="videoModel.cover" alt="avatar" />
+            <div v-else>
+              <a-icon style="font-size:24px;color:#b4bac2" type="picture" />
+              <div class="ant-upload-text">
+                上传封面
+              </div>
+            </div>
+          </a-upload>
+        </div>
+        <div style="margin-left: 85px;font-size:12px">清晰美观的封面有利于推荐，建议分辨率不低于1000*654（大小不超过2M）</div>
+        <div class="input-item">
+          <div style="width:50px; margin-right: 40px" for="title"><span style="color:red">* </span>
+            类别</div>
+          <a-select mode="multiple" :default-value="videoModel.category" style="width: 100%" placeholder="请选择视频类别"
+            @change="optionChange">
+            <a-select-option v-for="(item) in catogery" :key="item">
+              {{ item }}
+            </a-select-option>
+          </a-select>
+        </div>
+        <div class="input-item">
+          <div style="width:50px; margin-right: 40px" for="title"><span>* </span>描述</div>
+          <a-textarea v-model="videoModel.desc" :maxLength="120" placeholder="简单的介绍一下你的视频吧~" :rows="4" />
+        </div>
+        <div class="input-item">
+          <v-btn style="margin-left:85px" color="error" elevation="2">发布</v-btn>
+          <v-btn @click="cancel" style="margin-left:30px" color="#f8f8f8" elevation="2">取消</v-btn>
+        </div>
       </div>
     </div>
   </div>
@@ -90,43 +140,71 @@
 
 <script>
 import moment from 'moment';
+import { Base64 } from 'js-base64';
 export default {
   data () {
     return {
-      isUploaded: true,
-      viedoInfo: {},
-      percen: 0,
-      isComplete: false,
-      isSuccess: false
+      visible: true, // 是否显示alert
+      isUploaded: true, // 是否已导入视频或图片
+      videoInfo: {},  // 视频信息
+      videoModel: {
+        title: '',
+        desc: '',
+        category: ['生活'],
+        cover: '', // 封面
+        file: '' // url
+      }, // 保存到数据库的视频信息
+      percen: 0,  // 加载百分比
+      isComplete: false, // 是否完成加载
+      isSuccess: false, // 加载是否成功
+      isHaveTemplate: false, // 是否有模板
+      catogery: ['游戏', '音乐', '美食', '搞笑', '农人', '体育', '宠物', '科技', 'Vlog'],
     }
   },
+  beforeDestroy () { },
   methods: {
-    formatTime (time) {
-      return moment.duration(time)
+    handleClose () {
+      this.visible = false;
+    },
+    // 时间格式化
+    durationTrans (a) {
+      var b = ""
+      var h = parseInt(a / 3600),
+        m = parseInt(a % 3600 / 60),
+        s = parseInt(a % 3600 % 60);
+      if (h > 0) {
+        h = h < 10 ? '0' + h : h
+        b += h + ":"
+      }
+      m = m < 10 ? '0' + m : m
+      s = s < 10 ? '0' + s : s
+      b += m + ":" + s
+      return b;
     },
     changeUpload () {
       this.$refs.upload.$children[0].$children[0].$children[0].onClick();
     },
+    beforeUpload (file) {
+
+    },
     // 上传回调函数
     handleChange (info) {
       try {
+        this.getTimes(info.file)
         this.isUploaded = false
         const status = info.file.status;
-        console.log(info)
         if (status === 'uploading') {
           this.percen = info.file.percent
-          this.viedoInfo.name = info.file.name
-          this.viedoInfo.size = info.file.size
-          this.viedoInfo.timeStamp = info.event.timeStamp
+          this.videoInfo.name = info.file.name
+          this.videoInfo.size = info.event.total
           if (info.event.loaded) {
-            this.viedoInfo.loaded = info.event.loaded
+            this.videoInfo.loaded = info.event.loaded
           }
-          this.viedoInfo.lastModified = info.file.lastModified
         }
         if (status === 'done') {
           this.isComplete = true
           this.isSuccess = true
-          this.viedoInfo.size = info.file.size
+          this.videoModel.file = info.file.response.url
           // this.$message.success(`${info.file.name} 上传成功`);
         } else if (status === 'error') {
           this.isSuccess = false
@@ -154,6 +232,21 @@ export default {
         return temp + 'GB';
       }
     },
+    // 获取视频长度
+    getTimes (file) {
+      // //获取录音时长
+      let url = URL.createObjectURL(file.originFileObj);
+      // //经测试，发现audio也可获取视频的时长
+      let audioElement = new Audio(url);
+      let duration;
+      audioElement.addEventListener("loadedmetadata", _event => {
+        duration = audioElement.duration;
+        if (duration) {
+          this.videoInfo.duration = duration
+          console.log(this.videoInfo.duration)
+        }
+      });
+    },
     // 获取token
     getAuthHeaders () {
       try {
@@ -168,7 +261,81 @@ export default {
       } catch (error) {
         console.log(error)
       }
+    },
+    // 上传图片
+    handleChange2 (info) {
+      if (info.file.status === 'done') {
+        // Get this url from response in real world.
+        this.videoModel.cover = info.file.response.url;
+      }
+    },
+    beforeUpload (file) {
+      const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+      if (!isJpgOrPng) {
+        this.$message.error('只能上传图片!');
+      }
+      const isLt2M = file.size / 1024 / 1024 < 2;
+      if (!isLt2M) {
+        this.$message.error('图标大小必须小于2MB!');
+      }
+      return isJpgOrPng && isLt2M;
+    },
+    // 选择框改变
+    optionChange (value) {
+      this.videoModel.catogery = value
+    },
+    // 保存模板信息
+    saveTemplate () {
+      const videoTemplate = {
+        percen: this.percen,
+        videoModel: this.videoModel,
+        videoInfo: this.videoInfo,
+        isComplete: this.isComplete,
+        isSuccess: this.isSuccess
+      } // 保存到版本地的模板
+      const storage = window.localStorage
+      storage.setItem("video_template", Base64.encode(JSON.stringify(videoTemplate)));
+    },
+    //删除模板
+    deleteTemplate () {
+      const videoTemplate = {}
+      const storage = window.localStorage
+      // 删除本地存储 初始化数据
+      storage.removeItem("video_template");
+      this.videoInfo = {}
+      this.videoModel = {}
+      this.percen = ''
+      this.isComplete = false
+      this.isSuccess = false
+      this.isHaveTemplate = false
+    },
+    // 获取模板信息
+    getTemplate () {
+      const storage = window.localStorage
+      return JSON.parse(Base64.decode(storage.getItem("video_template")));
+    },
+    // 取消上传
+    cancel () {
+      //保存模板到本地存储
+      this.saveTemplate()
+      this.isUploaded = true
+      this.isHaveTemplate = true
     }
+  },
+  mounted () {
+    // 加载模板信息
+    const template = this.getTemplate()
+    if (template) {
+      this.isHaveTemplate = true
+      if (template.videoModel.file) {
+        this.videoInfo = template.videoInfo
+        this.videoModel = template.videoModel
+        this.percen = template.percen
+        this.isComplete = template.isComplete
+        this.isSuccess = template.isSuccess
+      }
+    }
+
   }
 }
 </script>
@@ -184,7 +351,7 @@ export default {
   justify-content: center
   .display
     width: 80%
-    height: 550px
+    height: 710px
     background-color: #fff
     border-radius: 7px
     margin: 0 auto
@@ -219,7 +386,7 @@ export default {
             color: red
   .wrap-center
     height: 484px
-    width: 900px
+    width: 80%
     border-radius: 7px
   .upload-dragg
     position: absolute
@@ -229,6 +396,18 @@ export default {
     width: 900px
     border: 2.5px dashed #efefef
     border-radius: 10px
+    .templateTips
+      height: 40px
+      width: 870px
+      background-color: #f8f8f8
+      margin: 10px auto
+      position: absolute
+      display: flex
+      align-item: center
+      line-height: 40px
+      left: 12px
+      z-index: 999
+      border-radius: 2px
   .upload-dragg:hover
     border: 2.5px dashed #b4bac2
   .create-bottom
@@ -237,7 +416,7 @@ export default {
     padding: 20px 0
   .create-wrap
     width: 900px
-    height: 500px
+    height: 484px
     border-radius: 7px
     background-color: #ffff
     margin-top: 46px
@@ -254,4 +433,11 @@ export default {
       .bottom-item
         display: flex
         align-items: center
+.video-form
+  color: grey
+  margin-top: 30px
+  .input-item
+    display: flex
+    align-item: center
+    margin-top: 30px
 </style>
